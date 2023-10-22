f1c764cea72c92420e20acad2b521e2a433c5e5c
Peter Zong <peterzong0525@gmail.com>
2023-10-22 01:20:59 -0500

    Implemented core table functionality

--- PasswordManager.py
+++ PasswordManager.py
@@ -19,6 +19,8 @@ class PasswordManager():
         self.ConnectButtons()
 
         self.loginData = pd.DataFrame(columns = ['Name', 'Username', 'Email', 'Password', 'Pin', 'Notes'])
+        self.action = ""
+
         self.GetData()
         self.DisplayTable()
 
@@ -36,49 +38,130 @@ class PasswordManager():
 
 
     def ConnectButtons(self):
-        #self.ui.pushButtonSearch.clicked.connect()
-        #self.ui.pushButtonClear.clicked.connect()
-        self.ui.pushButtonAddNewEntry.clicked.connect(self.AddNewEntry)
+        self.ui.pushButtonSearch.clicked.connect(self.OnFilterData)
+        self.ui.pushButtonClear.clicked.connect(self.OnClearFilters)
+        self.ui.pushButtonAddNewEntry.clicked.connect(self.OnAddNewEntry)
         self.ui.pushButtonEdit.clicked.connect(self.OnEdit)
-        #self.ui.pushButtonDelete.clicked.connect()
-        self.ui.pushButtonSave.clicked.connect(self.SaveEdits)
-        #self.ui.pushButtonCancel.clicked.connect()
-        self.ui.pushButtonExportData.clicked.connect(self.ExportData)
-        self.ui.pushButtonGenSecPass.clicked.connect(self.GeneratePassword)
+        self.ui.pushButtonDelete.clicked.connect(self.OnDelete)
+        self.ui.pushButtonSave.clicked.connect(self.OnSave)
+        self.ui.pushButtonCancel.clicked.connect(self.OnCancel)
+        self.ui.pushButtonExportData.clicked.connect(self.OnExportData)
+        self.ui.pushButtonGenSecPass.clicked.connect(self.OnGeneratePassword)
 
 
     def GetData(self):
-        if os.path.isfile("./Login_Data.pkl"):
-            self.loginData = pd.read_pickle("./Login_Data.pkl")
+        if os.path.isfile("./Login_Data.parquet"):
+            self.loginData = pd.read_parquet("./Login_Data.parquet")
         
     def SaveData(self):
-        self.loginData.to_pickle("./Login_Data.pkl")
+        self.loginData.to_parquet("./Login_Data.parquet")
 
-    def ExportData(self):
+    def OnExportData(self):
         self.loginData.to_excel("./Login_Data.xlsx", index=False)
 
 
-    def OnEdit(self):
+    def OnFilterData(self):
+        name = self.ui.lineEditNameSearch.text().lower()
+        email = self.ui.lineEditEmailSearch.text().lower()
+
+        for row in range(self.ui.tableWidgetLoginData.rowCount()):
+            itemName = self.ui.tableWidgetLoginData.item(row, 0)
+            itemEmail = self.ui.tableWidgetLoginData.item(row, 2)
+            itemNameText = itemName.text().lower() if itemName else ''
+            itemEmailText = itemEmail.text().lower() if itemEmail else ''
+
+            if (name in itemNameText) and (email in itemEmailText):
+                self.ui.tableWidgetLoginData.setRowHidden(row, False)
+            else:
+                self.ui.tableWidgetLoginData.setRowHidden(row, True)
+
+    def OnClearFilters(self):
+        print("Clearing Filters")
+        self.ui.lineEditNameSearch.setText('')
+        self.ui.lineEditEmailSearch.setText('')
+
+        self.OnFilterData()
+
+
+    def EnterEditDeleteUI(self):
         self.ui.pushButtonEdit.setVisible(False)
         self.ui.pushButtonDelete.setVisible(False)
         self.ui.pushButtonCancel.setVisible(True)
         self.ui.pushButtonSave.setVisible(True)
-
-        self.ui.tableWidgetLoginData.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.AllEditTriggers)
-
-
-    def SaveEdits(self):
+        
+        self.ui.lineEditNameSearch.setDisabled(True)
+        self.ui.lineEditEmailSearch.setDisabled(True)
+        self.ui.pushButtonSearch.setDisabled(True)
+        self.ui.pushButtonClear.setDisabled(True)
+        self.ui.lineEditNameNewEntry.setDisabled(True)
+        self.ui.lineEditUsernameNewEntry.setDisabled(True)
+        self.ui.lineEditEmailNewEntry.setDisabled(True)
+        self.ui.lineEditPasswordNewEntry.setDisabled(True)
+        self.ui.pushButtonGenSecPass.setDisabled(True)
+        self.ui.lineEditPinNewEntry.setDisabled(True)
+        self.ui.lineEditNotesNewEntry.setDisabled(True)
+        self.ui.pushButtonAddNewEntry.setDisabled(True)
+        self.ui.pushButtonExportData.setDisabled(True)
+        
+    def ExitEditDeleteUI(self):
         self.ui.pushButtonEdit.setVisible(True)
         self.ui.pushButtonDelete.setVisible(True)
         self.ui.pushButtonCancel.setVisible(False)
         self.ui.pushButtonSave.setVisible(False)
-
+        
+        self.ui.lineEditNameSearch.setEnabled(True)
+        self.ui.lineEditEmailSearch.setEnabled(True)
+        self.ui.pushButtonSearch.setEnabled(True)
+        self.ui.pushButtonClear.setEnabled(True)
+        self.ui.lineEditNameNewEntry.setEnabled(True)
+        self.ui.lineEditUsernameNewEntry.setEnabled(True)
+        self.ui.lineEditEmailNewEntry.setEnabled(True)
+        self.ui.lineEditPasswordNewEntry.setEnabled(True)
+        self.ui.pushButtonGenSecPass.setEnabled(True)
+        self.ui.lineEditPinNewEntry.setEnabled(True)
+        self.ui.lineEditNotesNewEntry.setEnabled(True)
+        self.ui.pushButtonAddNewEntry.setEnabled(True)
+        self.ui.pushButtonExportData.setEnabled(True)
+        
         self.ui.tableWidgetLoginData.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.ui.tableWidgetLoginData.setSelectionBehavior(QtWidgets.QTableWidget.SelectionBehavior.SelectItems)
+        self.ui.tableWidgetLoginData.clearSelection()
 
-        self.ApplyEdits()
+
+    def OnEdit(self):
+        self.EnterEditDeleteUI()
+        self.ui.tableWidgetLoginData.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.AllEditTriggers)
+        self.ui.tableWidgetLoginData.setSelectionBehavior(QtWidgets.QTableWidget.SelectionBehavior.SelectItems)
+        self.action = "Edit"
+
+    def OnSave(self):
+        print("Saving changes")
+        self.ExitEditDeleteUI()
+
+        if self.action == "Edit":
+            self.ApplyEdits()
+        elif self.action == "Delete":
+            self.DeleteRows()
+
+        self.action = ""
+
+    def OnCancel(self):
+        print("Cancel changes")
+        self.ExitEditDeleteUI()
+
+        if self.action == "Edit":
+            self.DisplayTable()
+
+        self.action = ""
+
+    def OnDelete(self):
+        self.EnterEditDeleteUI()
+        self.ui.tableWidgetLoginData.setSelectionBehavior(QtWidgets.QTableWidget.SelectionBehavior.SelectRows)
+        self.action = "Delete"
+        
 
     
-    def AddNewEntry(self):
+    def OnAddNewEntry(self):
         # Perform checks that the necessary fields are filled in
         
         newEntryData = {
@@ -102,7 +185,7 @@ class PasswordManager():
         self.SaveData()
 
 
-    def _lastColumnFits(self):
+    def lastColumnFits(self):
         total_width = self.ui.tableWidgetLoginData.viewport().width()
         column_width_sum = 0
 
@@ -112,7 +195,6 @@ class PasswordManager():
         return total_width >= column_width_sum
     
     def DisplayTable(self):
-        print("Displaying")
         self.ui.tableWidgetLoginData.setRowCount(self.loginData.shape[0])
         self.ui.tableWidgetLoginData.setColumnCount(self.loginData.shape[1])
         self.ui.tableWidgetLoginData.setHorizontalHeaderLabels(self.loginData.columns)        
@@ -126,14 +208,12 @@ class PasswordManager():
 
         self.ui.tableWidgetLoginData.resizeColumnsToContents()
         
-        if self._lastColumnFits():
+        if self.lastColumnFits():
             self.ui.tableWidgetLoginData.horizontalHeader().setSectionResizeMode(self.ui.tableWidgetLoginData.columnCount() - 1, QtWidgets.QHeaderView.ResizeMode.Stretch)
             self.ui.tableWidgetLoginData.horizontalHeader().setStretchLastSection(True)
 
 
-    def ApplyEdits(self):
-        print("Applying edits")
-        
+    def ApplyEdits(self):       
         numRows = self.ui.tableWidgetLoginData.rowCount()
         numColumns = self.ui.tableWidgetLoginData.columnCount()
 
@@ -153,7 +233,24 @@ class PasswordManager():
         self.DisplayTable()
 
 
-    def GeneratePassword(self):
+    def DeleteRows(self):
+        print("Deleting")
+
+        selectedRows = []
+        for item in self.ui.tableWidgetLoginData.selectedItems():
+            selectedRows.append(item.row())
+        selectedRows = sorted(selectedRows, reverse = True)
+
+        self.loginData = self.loginData.drop(index = selectedRows)
+
+        self.ui.tableWidgetLoginData.clearSelection()
+
+        for row in selectedRows:
+            self.ui.tableWidgetLoginData.removeRow(row)
+
+
+
+    def OnGeneratePassword(self):
         # Open new password window
 
         PasswordObj = PasswordGenerator()
